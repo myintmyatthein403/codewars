421855b70d9e6a6e4dc7823fce8f47d5c1fa65a2
Myint Myat Thein <myintmyatthein403@gmail.com>
2025-04-25 16:13:37 +0700

    Rust Practice

--- 8 kyu/Switch it Up/Rust/main.rs
+++ 8 kyu/Switch it Up/Rust/main.rs
@@ -5,16 +5,16 @@ fn main() {
 
 fn switch_it_up(n: usize) -> &'static str {
     match n {
-        1=>"one",
-        2=>"two",
-        3=>"three",
-        4=>"four",
-        5=>"five",
-        6=>"six",
-        7=>"seven",
-        8=>"eight",
-        9=>"nine",
-        0=>"zero",
+        1=>"One",
+        2=>"Two",
+        3=>"Three",
+        4=>"Four",
+        5=>"Five",
+        6=>"Six",
+        7=>"Seven",
+        8=>"Eight",
+        9=>"Nine",
+        0=>"Zero",
         _=>"",
     }
 }
